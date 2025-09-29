1dec6cf041270ea0f7d1981382405d867be12d1d
Chris Jenkins <cdpjenkins@gmail.com>
2025-09-29 21:50:30 +0100

    feat: filter out non-alphanumeric characters

--- src/test/java/PalindromeRadarTest.java
+++ src/test/java/PalindromeRadarTest.java
@@ -1,25 +1,39 @@
 import org.junit.jupiter.api.Test;
 
+import static java.util.stream.Collectors.joining;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class PalindromeRadarTest {
 
+    PalindromeRadar palindromeRadar = new PalindromeRadar();
+
     @Test
     void simple_palindrome_with_only_lower_case_letters_is_recognised() {
-        assertThat(new PalindromeRadar().isPalindrome("anna"), is(true));
+        assertThat(palindromeRadar.isPalindrome("anna"), is(true));
     }
 
     @Test
     void non_palindrome_is_recognised_as_not_palindrome() {
-        assertThat(new PalindromeRadar().isPalindrome("notpalindrome"), is(false));
+        assertThat(palindromeRadar.isPalindrome("notpalindrome"), is(false));
+    }
+
+    @Test
+    void ignore_non_alphanumeric_chars() {
+        assertThat(palindromeRadar.isPalindrome("rad ar"), is(true));
     }
 }
 
 class PalindromeRadar {
 
     public boolean isPalindrome(String input) {
-        return input.equals(reverse(input));
+        String alphaNumericInput = filterAlphanumeric(input);
+
+        return alphaNumericInput.equals(reverse(alphaNumericInput));
+    }
+
+    private static String filterAlphanumeric(String input) {
+        return input.replaceAll("[^a-zA-Z0-9]", "");
     }
 
     private static String reverse(String input) {
