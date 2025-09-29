d43e8646d7a4c42421c2c27c46aa9e27381fbf5e
Chris Jenkins <cdpjenkins@gmail.com>
2025-09-29 21:43:21 +0100

    feat: find palindromes
    Not yet ignoring case or non-alphanumeric chars.

--- src/test/java/PalindromeRadarTest.java
+++ src/test/java/PalindromeRadarTest.java
@@ -7,13 +7,22 @@ public class PalindromeRadarTest {
 
     @Test
     void simple_palindrome_with_only_lower_case_letters_is_recognised() {
-        assertThat(new PalindromeRadar().isPalindrome(), is(true));
+        assertThat(new PalindromeRadar().isPalindrome("anna"), is(true));
+    }
+
+    @Test
+    void non_palindrome_is_recognised_as_not_palindrome() {
+        assertThat(new PalindromeRadar().isPalindrome("notpalindrome"), is(false));
     }
 }
 
 class PalindromeRadar {
 
-    public boolean isPalindrome() {
-        return true;
+    public boolean isPalindrome(String input) {
+        return input.equals(reverse(input));
+    }
+
+    private static String reverse(String input) {
+        return new StringBuilder(input).reverse().toString();
     }
 }
