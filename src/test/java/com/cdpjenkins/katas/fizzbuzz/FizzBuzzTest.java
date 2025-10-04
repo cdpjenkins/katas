ce0b95cfc66ceca0176ebb12edb6eab15b91c5a7
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-04 10:16:27 +0100

    feat: convert 1 to 1

--- src/test/java/com/cdpjenkins/katas/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/cdpjenkins/katas/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,19 @@
 package com.cdpjenkins.katas.fizzbuzz;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class FizzBuzzTest {
+    @Test
+    void converts_1_to_1() {
+        assertThat(new FizzBuzz().convert(1), Matchers.is("1"));
+    }
+}
+
+class FizzBuzz {
+    public String convert(int i) {
+        return "1";
+    }
 }
