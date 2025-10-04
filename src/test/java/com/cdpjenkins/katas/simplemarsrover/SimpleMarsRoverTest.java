e43639df0d4ded4bdcaebcfdc4523412459a54fc
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-04 11:38:44 +0100

    feat(simplemarsrover): handle moving north by 1

--- src/test/java/com/cdpjenkins/katas/simplemarsrover/SimpleMarsRoverTest.java
+++ src/test/java/com/cdpjenkins/katas/simplemarsrover/SimpleMarsRoverTest.java
@@ -11,10 +11,21 @@ public class SimpleMarsRoverTest {
     void rover_starts_at_position_0_0_facing_north() {
         assertThat(MarsRover.execute(""), is("0:0:N"));
     }
+
+    @Test
+    void y_coord_is_incremented_by_1_when_we_move_north() {
+        assertThat(MarsRover.execute("M"), is("0:1:N"));
+    }
 }
 
 class MarsRover {
     public static String execute(String command) {
-        return "0:0:N";
+        int y = 0;
+
+        if (!command.isEmpty()) {
+            y++;
+        }
+
+        return String.format("0:%d:N", y);
     }
 }
