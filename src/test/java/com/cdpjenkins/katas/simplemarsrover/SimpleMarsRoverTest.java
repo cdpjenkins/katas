7f8bc125916929869fe5f0e69252ff82a6e5bc6e
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-04 11:45:07 +0100

    feat(simplemarsrover): rover wraps when we reach top of grid

--- src/test/java/com/cdpjenkins/katas/simplemarsrover/SimpleMarsRoverTest.java
+++ src/test/java/com/cdpjenkins/katas/simplemarsrover/SimpleMarsRoverTest.java
@@ -21,6 +21,11 @@ public class SimpleMarsRoverTest {
     void y_coord_is_incremented_by_2_when_we_move_north_twice() {
         assertThat(MarsRover.execute("MM"), is("0:2:N"));
     }
+
+    @Test
+    void rover_wraps_around_when_we_reach_the_top_of_the_grid() {
+        assertThat(MarsRover.execute("MMMMMMMMMM"), is("0:0:N"));
+    }
 }
 
 class MarsRover {
@@ -29,6 +34,9 @@ class MarsRover {
 
         for (char command : commands.toCharArray()) {
             y++;
+            if (y >= 10) {
+                y = 0;
+            }
         }
 
         return String.format("0:%d:N", y);
