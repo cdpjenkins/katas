bea5a166f64c778889b0f0881b0fc8b369320806
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-04 12:03:07 +0100

    feat(simplemarsrover): handle the other cases of turning right

--- src/test/java/com/cdpjenkins/katas/simplemarsrover/SimpleMarsRoverTest.java
+++ src/test/java/com/cdpjenkins/katas/simplemarsrover/SimpleMarsRoverTest.java
@@ -56,18 +56,42 @@ public class SimpleMarsRoverTest {
     void rover_faces_south_after_turning_right_twice() {
         assertThat(MarsRover.execute("RR"), is("0:0:S"));
     }
+
+    @Test
+    void rover_faces_west_after_turning_right_three_times() {
+        assertThat(MarsRover.execute("RRR"), is("0:0:W"));
+    }
+
+    @Test
+    void rover_faces_north_again_after_turning_right_fourth_times() {
+        assertThat(MarsRover.execute("RRRR"), is("0:0:N"));
+    }
+
+    @Test
+    void x_coord_is_incremented_by_1_when_we_move_east() {
+        assertThat(MarsRover.execute("RM"), is("1:0:E"));
+    }
 }
 
 class MarsRover {
     public static String execute(String commands) {
         char direction = 'N';
+        int x = 0;
         int y = 0;
 
         for (char command : commands.toCharArray()) {
             if (command == 'M') {
-                y++;
-                if (y >= 10) {
-                    y = 0;
+
+                switch (direction) {
+                    case 'N' -> {
+                        y++;
+                        if (y >= 10) {
+                            y = 0;
+                        }
+                    }
+                    case 'E' -> {
+                        x++;
+                    }
                 }
             } else if (command == 'L') {
                 direction = switch (direction) {
@@ -81,11 +105,13 @@ class MarsRover {
                 direction = switch (direction) {
                     case 'N' -> 'E';
                     case 'E' -> 'S';
+                    case 'S' -> 'W';
+                    case 'W' -> 'N';
                     default -> direction;
                 };
             }
         }
 
-        return String.format("0:%d:%c", y, direction);
+        return String.format("%d:%d:%c", x, y, direction);
     }
 }
