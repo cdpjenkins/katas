46844f829c5b5cb9f5f245ff9780e0c8e32bb07e
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-04 12:34:49 +0100

    refactor(simplemarsrover):  rename Position record to MarsRover because we ultimately want it to encapsulate position as well

--- src/test/java/com/cdpjenkins/katas/simplemarsrover/SimpleMarsRoverTest.java
+++ src/test/java/com/cdpjenkins/katas/simplemarsrover/SimpleMarsRoverTest.java
@@ -11,82 +11,82 @@ public class SimpleMarsRoverTest {
 
     @Test
     void rover_starts_at_position_0_0_facing_north() {
-        assertThat(MarsRover.execute(""), is("0:0:N"));
+        assertThat(MarsRoverExecutor.execute(""), is("0:0:N"));
     }
 
     @Test
     void y_coord_is_incremented_by_1_when_we_move_north() {
-        assertThat(MarsRover.execute("M"), is("0:1:N"));
+        assertThat(MarsRoverExecutor.execute("M"), is("0:1:N"));
     }
 
     @Test
     void y_coord_is_incremented_by_2_when_we_move_north_twice() {
-        assertThat(MarsRover.execute("MM"), is("0:2:N"));
+        assertThat(MarsRoverExecutor.execute("MM"), is("0:2:N"));
     }
 
     @Test
     void rover_wraps_around_when_we_reach_the_top_of_the_grid() {
-        assertThat(MarsRover.execute("MMMMMMMMMM"), is("0:0:N"));
+        assertThat(MarsRoverExecutor.execute("MMMMMMMMMM"), is("0:0:N"));
     }
 
     @Test
     void rover_faces_west_after_turning_left() {
-        assertThat(MarsRover.execute("L"), is("0:0:W"));
+        assertThat(MarsRoverExecutor.execute("L"), is("0:0:W"));
     }
 
     @Test
     void rover_faces_south_after_turning_left_twice() {
-        assertThat(MarsRover.execute("LL"), is("0:0:S"));
+        assertThat(MarsRoverExecutor.execute("LL"), is("0:0:S"));
     }
 
     @Test
     void rover_faces_east_after_turning_left_three_times() {
-        assertThat(MarsRover.execute("LLL"), is("0:0:E"));
+        assertThat(MarsRoverExecutor.execute("LLL"), is("0:0:E"));
     }
 
     @Test
     void rover_faces_north_again_after_turning_left_fourth_times() {
-        assertThat(MarsRover.execute("LLLL"), is("0:0:N"));
+        assertThat(MarsRoverExecutor.execute("LLLL"), is("0:0:N"));
     }
 
     @Test
     void rover_faces_east_after_turning_right() {
-        assertThat(MarsRover.execute("R"), is("0:0:E"));
+        assertThat(MarsRoverExecutor.execute("R"), is("0:0:E"));
     }
 
     @Test
     void rover_faces_south_after_turning_right_twice() {
-        assertThat(MarsRover.execute("RR"), is("0:0:S"));
+        assertThat(MarsRoverExecutor.execute("RR"), is("0:0:S"));
     }
 
     @Test
     void rover_faces_west_after_turning_right_three_times() {
-        assertThat(MarsRover.execute("RRR"), is("0:0:W"));
+        assertThat(MarsRoverExecutor.execute("RRR"), is("0:0:W"));
     }
 
     @Test
     void rover_faces_north_again_after_turning_right_fourth_times() {
-        assertThat(MarsRover.execute("RRRR"), is("0:0:N"));
+        assertThat(MarsRoverExecutor.execute("RRRR"), is("0:0:N"));
     }
 
     @Test
     void x_coord_is_incremented_by_1_when_we_move_east() {
-        assertThat(MarsRover.execute("RM"), is("1:0:E"));
+        assertThat(MarsRoverExecutor.execute("RM"), is("1:0:E"));
     }
 
     @Test
     void rover_wraps_around_when_we_reach_the_right_edge_of_the_grid() {
-        assertThat(MarsRover.execute("RMMMMMMMMMM"), is("0:0:E"));
+        assertThat(MarsRoverExecutor.execute("RMMMMMMMMMM"), is("0:0:E"));
     }
 
     @Test
     void rover_can_move_south_and_wraps_around_when_we_reach_the_bottom_edge_of_the_grid() {
-        assertThat(MarsRover.execute("LLM"), is("0:9:S"));
+        assertThat(MarsRoverExecutor.execute("LLM"), is("0:9:S"));
     }
 
     @Test
     void rover_can_move_west_and_wraps_around_when_we_reach_the_left_edge_of_the_grid() {
-        assertThat(MarsRover.execute("LM"), is("9:0:W"));
+        assertThat(MarsRoverExecutor.execute("LM"), is("9:0:W"));
     }
 
     @ParameterizedTest
@@ -96,12 +96,11 @@ public class SimpleMarsRoverTest {
             "RMMLM, 2:1:N"
     })
     void rover_moves_correctly_when_given_more_complex_commands(String commands, String finalState) {
-        assertThat(MarsRover.execute(commands), is(finalState));
+        assertThat(MarsRoverExecutor.execute(commands), is(finalState));
     }
 }
 
-record Position(int x, int y) { }
-
+record MarsRover(int x, int y) { }
 
 enum Command {
     MOVE,
@@ -118,18 +117,18 @@ enum Command {
     }
 }
 
-class MarsRover {
+class MarsRoverExecutor {
     public static String execute(String commands) {
         char direction = 'N';
 
-        Position position = new Position(0, 0);
+        MarsRover marsRover = new MarsRover(0, 0);
 
         for (char c : commands.toCharArray()) {
 
             Command command = Command.fromChar(c);
 
             switch (command) {
-                case MOVE -> position = move(position, direction);
+                case MOVE -> marsRover = move(marsRover, direction);
                 case TURN_LEFT -> direction = switch (direction) {
                     case 'N' -> 'W';
                     case 'W' -> 'S';
@@ -147,12 +146,12 @@ class MarsRover {
             }
         }
 
-        return String.format("%d:%d:%c", position.x(), position.y(), direction);
+        return String.format("%d:%d:%c", marsRover.x(), marsRover.y(), direction);
     }
 
-    private static Position move(Position position, char direction) {
-        int x = position.x();
-        int y = position.y();
+    private static MarsRover move(MarsRover marsRover, char direction) {
+        int x = marsRover.x();
+        int y = marsRover.y();
 
         switch (direction) {
             case 'N' -> {
@@ -181,7 +180,7 @@ class MarsRover {
             }
         }
 
-        position = new Position(x, y);
-        return position;
+        marsRover = new MarsRover(x, y);
+        return marsRover;
     }
 }
