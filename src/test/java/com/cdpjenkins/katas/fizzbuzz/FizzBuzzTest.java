2efee6b2df6978e79ec518dd6633a7f051441274
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-04 11:14:40 +0100

    refactor: rename input parameter used in tests to match the language in the problem statement

--- src/test/java/com/cdpjenkins/katas/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/cdpjenkins/katas/fizzbuzz/FizzBuzzTest.java
@@ -12,34 +12,34 @@ public class FizzBuzzTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 4, 7, 8})
-    void converts_number_that_is_not_divisible_by_3_or_5_to_string_representation_of_that_number(int input) {
-        assertThat(fizzBuzz.convert(input), is(input + ""));
+    void converts_number_that_is_not_divisible_by_3_or_5_to_string_representation_of_that_number(int number) {
+        assertThat(fizzBuzz.convert(number), is(number + ""));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 12, 18, 21})
-    void converts_number_that_is_divisible_by_3_but_not_by_5_to_Fizz(int input) {
-        assertThat(fizzBuzz.convert(input), is("Fizz"));
+    void converts_number_that_is_divisible_by_3_but_not_by_5_to_Fizz(int number) {
+        assertThat(fizzBuzz.convert(number), is("Fizz"));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 10, 20, 25, 35, 40})
-    void converts_number_that_is_divisible_by_5_but_not_by_3_to_Buzz(int input) {
-        assertThat(fizzBuzz.convert(input), is("Buzz"));
+    void converts_number_that_is_divisible_by_5_but_not_by_3_to_Buzz(int number) {
+        assertThat(fizzBuzz.convert(number), is("Buzz"));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {15, 30, 45, 60, 75, 90})
-    void converts_number_that_is_divisible_by_both_3_and_5_to_FizzBuzz(int input) {
-        assertThat(fizzBuzz.convert(input), is("FizzBuzz"));
+    void converts_number_that_is_divisible_by_both_3_and_5_to_FizzBuzz(int number) {
+        assertThat(fizzBuzz.convert(number), is("FizzBuzz"));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, -1, -2, -3, -10000000, Integer.MIN_VALUE})
-    void throws_IllegalArguementException_when_input_is_not_positive(int input) {
+    void throws_IllegalArguementException_when_input_is_not_positive(int number) {
         assertThrows(
                 IllegalArgumentException.class,
-                () -> fizzBuzz.convert(input)
+                () -> fizzBuzz.convert(number)
         );
     }
 }
