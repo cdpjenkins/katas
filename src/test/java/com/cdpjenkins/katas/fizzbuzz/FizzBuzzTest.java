27def08224a49dfb58a5d55bb360ac6c605d91fc
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-04 10:26:58 +0100

    feat: convert 3 to Buzz

--- src/test/java/com/cdpjenkins/katas/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/cdpjenkins/katas/fizzbuzz/FizzBuzzTest.java
@@ -4,21 +4,33 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
     void converts_1_to_1() {
-        assertThat(new FizzBuzz().convert(1), Matchers.is("1"));
+        assertThat(fizzBuzz.convert(1), is("1"));
     }
 
     @Test
     void converts_2_to_2() {
-        assertThat(new FizzBuzz().convert(2), Matchers.is("2"));
+        assertThat(fizzBuzz.convert(2), is("2"));
+    }
+
+    @Test
+    void converts_3_to_Fizz() {
+        assertThat(fizzBuzz.convert(3), is("Fizz"));
     }
 }
 
 class FizzBuzz {
     public String convert(int number) {
-        return Integer.toString(number);
+        if (number == 3) {
+            return "Fizz";
+        } else {
+            return Integer.toString(number);
+        }
     }
 }
