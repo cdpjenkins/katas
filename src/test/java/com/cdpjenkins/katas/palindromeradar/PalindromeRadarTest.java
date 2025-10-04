9753ff9a872b56f63aa41e2c554b7a952f5149f3
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-04 10:10:39 +0100

    refactor: move Palindrome Radar kata into its own package

--- src/test/java/com/cdpjenkins/katas/palindromeradar/PalindromeRadarTest.java
+++ src/test/java/com/cdpjenkins/katas/palindromeradar/PalindromeRadarTest.java
@@ -1,3 +1,5 @@
+package com.cdpjenkins.katas.palindromeradar;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
