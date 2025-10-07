ca8518eebbc73d6db367af615dfb52be58010a2f
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-07 22:10:23 +0100

    feat(smartfridge): Start Smart Fridge kata

---
+++ src/test/java/com/cdpjenkins/katas/smartfridge/SmartFridgeTest.java
@@ -0,0 +1,32 @@
+package com.cdpjenkins.katas.smartfridge;
+
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SmartFridgeTest {
+
+
+
+    @Test
+    void fridge_is_initially_empty() {
+        SmartFridge smartFridge = new SmartFridge();
+
+        String contents = smartFridge.formatContents();
+
+        assertThat(contents, isOutput(""));
+    }
+
+    private static Matcher<String> isOutput(String expectedOutput) {
+        return is(expectedOutput.stripIndent().trim());
+    }
+
+}
+
+class SmartFridge {
+    public String formatContents() {
+        return "";
+    }
+}
