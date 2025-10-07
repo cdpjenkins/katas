10e35397143f923cf31de3bded9451206b24d833
Chris Jenkins <cdpjenkins@gmail.com>
2025-10-07 22:12:03 +0100

    feat(smartfridge): Fridge door is initially closed

--- src/test/java/com/cdpjenkins/katas/smartfridge/SmartFridgeTest.java
+++ src/test/java/com/cdpjenkins/katas/smartfridge/SmartFridgeTest.java
@@ -8,8 +8,6 @@ import static org.hamcrest.Matchers.is;
 
 public class SmartFridgeTest {
 
-
-
     @Test
     void fridge_is_initially_empty() {
         SmartFridge smartFridge = new SmartFridge();
@@ -19,14 +17,24 @@ public class SmartFridgeTest {
         assertThat(contents, isOutput(""));
     }
 
+    @Test
+    void fridge_door_is_initially_closed() {
+        SmartFridge smartFridge = new SmartFridge();
+
+        assertThat(smartFridge.isDoorOpen(), is(false));
+    }
+
     private static Matcher<String> isOutput(String expectedOutput) {
         return is(expectedOutput.stripIndent().trim());
     }
-
 }
 
 class SmartFridge {
     public String formatContents() {
         return "";
+    }
+
+    public boolean isDoorOpen() {
+        return false;
     }
 }
